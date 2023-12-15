50fd3f965eabe110e2351db82ee5716748974f07
frunolu <fruno.lu@gmail.com>
2023-12-15 13:02:13 +0100

    edit HomePresenter.php

--- project/app/Presenters/HomePresenter.php
+++ project/app/Presenters/HomePresenter.php
@@ -4,9 +4,45 @@ declare(strict_types=1);
 
 namespace App\Presenters;
 
+use App\Model\HtmlElement\FormElement;
+use App\Builders\FormBuilder;
 use Nette;
+use Nette\Application\UI\Form as NetteForm;
 
-
-final class HomePresenter extends Nette\Application\UI\Presenter
+class HomePresenter extends Nette\Application\UI\Presenter
 {
+    /**
+     * @var FormBuilder
+     */
+    private FormBuilder $formBuilder;
+
+    /**
+     * HomePresenter constructor.
+     *
+     * @param FormBuilder $formBuilder
+     */
+    public function __construct(FormBuilder $formBuilder)
+    {
+        parent::__construct();
+        $this->formBuilder = $formBuilder;
+    }
+
+    private function buildForm(): FormElement
+    {
+        return $this->formBuilder->buildForm();
+    }
+
+    protected function createComponentMyForm(): NetteForm
+    {
+        $formElement = $this->buildForm();
+        $form = new NetteForm;
+        $form->setAction($this->link('submit'));
+
+        return $form;
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->myForm = $this['myForm'];
+    }
 }
