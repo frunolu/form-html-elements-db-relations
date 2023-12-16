ea5841f145e59e4a0f66a3787185c6ac8274baef
frunolu <fruno.lu@gmail.com>
2023-12-16 15:00:42 +0100

    ./vendor/bin/phpcs -a  app

--- project/app/Builders/FormBuilder.php
+++ project/app/Builders/FormBuilder.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Builders;
 
 use App\Model\HtmlElement\AElement;
@@ -18,34 +20,47 @@ use Exception;
 class FormBuilder
 {
     /**
-     * @return FormElement
+     * Create and render the form
+     *
      * @throws Exception
+     *
+     * @return FormElement
      */
     public function buildForm(): FormElement
     {
         $form = $this->createView();
         $form = $this->addFormItems($form);
+
         echo $form->render();
+
         return $form;
     }
 
     /**
-     * @param  FormElement $form
-     * @return FormElement
+     * Add items to the form
+     *
+     * @param FormElement $form
+     *
      * @throws Exception
+     *
+     * @return FormElement
      */
     private function addFormItems(FormElement $form): FormElement
     {
-        $form->addFormItem($this->createInputElement('text', 'username', 'Enter your username'))
+        $form->addFormItem($this->createInputElement('text','username','Enter your username'))
             ->addFormItem($this->createDivElement())
             ->addFormItem(
-                $this->createSelectElement('gender', ['male' => 'Male', 'female' => 'Female', 'other' => 'Other'])
+                $this->createSelectElement(
+                    'gender',
+                    ['male' => 'Male','female' => 'Female','other' => 'Other']
+                )
             )
-
             ->addFormItem($this->createDivElement())
-            ->addFormItem($this->createInputElement('submit', 'submit', 'Submit'))
+            ->addFormItem(
+                $this->createInputElement('submit','submit','Submit')
+            )
             ->addFormItem($this->createDivElement())
-            ->addFormItem($this->createAElement('https://www.billboard.com/charts/hot-100/', 'music-charts'))
+            ->addFormItem($this->createAElement('https://www.billboard.com/charts/hot-100/','music-charts'))
             ->addFormItem($this->createDivElement())
             ->addFormItem($this->createImageElement('project/www/img/musical-notes.jpg'));
 
@@ -53,35 +68,48 @@ class FormBuilder
     }
 
     /**
+     * Create FormElement view
+     *
      * @return FormElement
      */
     private function createView(): FormElement
     {
-        return (new FormElement())->setAction('/home')->setAttribute('method', 'post');
+        $formElement = new FormElement();
+        return $formElement->setAction('/home')->setAttribute('method', 'post');
     }
 
     /**
-     * @param  string $type
-     * @param  string $name
-     * @param  string $placeholder
+     * Create input element
+     *
+     * @param string $type
+     * @param string $name
+     * @param string $placeholder
+     *
      * @return InputElement
      */
     private function createInputElement(string $type, string $name, string $placeholder): InputElement
     {
-        return (new InputElement())->buildInputElement($type, $name, $placeholder);
+        $inputElement = new InputElement();
+
+        return $inputElement->buildInputElement($type, $name, $placeholder);
     }
 
     /**
-     * @param  string $name
-     * @param  array  $options
+     * @param string $name
+     * @param array  $options
+     *
      * @return SelectElement
      */
     private function createSelectElement(string $name, array $options): SelectElement
     {
-        return (new SelectElement())->buildSelectElement($name, $options);
+        $selectElement = new SelectElement();
+
+        return $selectElement->buildSelectElement($name, $options);
     }
 
     /**
+     * Create div element
+     *
      * @return DivElement
      */
     private function createDivElement(): DivElement
@@ -90,23 +118,32 @@ class FormBuilder
     }
 
     /**
-     * @param  string $href
-     * @param  string $text
+     * Create anchor element
+     *
+     * @param string $href
+     * @param string $text
+     *
      * @return AElement
      */
     private function createAElement(string $href, string $text): AElement
     {
-        return (new AElement())->buildAElement($href, $text);
+        $aElement = new AElement();
+        return $aElement->buildAElement($href, $text);
     }
 
     /**
-     * @param  string $src
-     * @return ImageElement
+     * Create image element
+     *
+     * @param string $src
+     *
      * @throws Exception
+     *
+     * @return ImageElement
      */
     private function createImageElement(string $src): ImageElement
     {
-        $imageElement = (new ImageElement())->setSrc($src);
+        $imageElement = new ImageElement();
+        $imageElement->setSrc($src);
 
         if ($imageElement->getSrc() !== $src) {
             throw new Exception('The image source has not been set properly.');
