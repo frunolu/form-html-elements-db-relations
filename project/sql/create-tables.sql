744a945269b81f91cbaab2dec452669e149567c7
frunolu <fruno.lu@gmail.com>
2024-01-07 08:48:14 +0100

    Add sql files

---
+++ project/sql/create-tables.sql
@@ -0,0 +1,61 @@
+-- Vytvoření tabulky typ_zanr
+CREATE TABLE IF NOT EXISTS typ_zanr (
+                                        id_typ_zanr INT PRIMARY KEY AUTO_INCREMENT,
+                                        nazev VARCHAR(50) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- Vytvoření tabulky typ_narodnost
+CREATE TABLE IF NOT EXISTS typ_narodnost (
+                                             id_typ_narodnost INT PRIMARY KEY AUTO_INCREMENT,
+                                             nazev VARCHAR(50) NOT NULL
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- Vytvoření tabulky interpret
+CREATE TABLE IF NOT EXISTS interpret (
+                                         id_interpret INT PRIMARY KEY AUTO_INCREMENT,
+                                         nazev VARCHAR(50) NOT NULL,
+    id_typ_narodnost INT,
+    FOREIGN KEY (id_typ_narodnost) REFERENCES typ_narodnost(id_typ_narodnost)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- Vytvoření tabulky album
+CREATE TABLE IF NOT EXISTS album (
+                                     id_album INT PRIMARY KEY AUTO_INCREMENT,
+                                     id_typ_zanr INT,
+                                     nazev VARCHAR(50) NOT NULL,
+    datum_vydani DATE,
+    FOREIGN KEY (id_typ_zanr) REFERENCES typ_zanr(id_typ_zanr)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- Vytvoření tabulky skladba
+CREATE TABLE IF NOT EXISTS skladba (
+                                       id_skladba INT PRIMARY KEY AUTO_INCREMENT,
+                                       nazev VARCHAR(50) NOT NULL,
+    delka INT
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- Vytvoření spojovací tabulky album_interpret
+CREATE TABLE IF NOT EXISTS album_interpret (
+                                               id_album_interpret INT PRIMARY KEY AUTO_INCREMENT,
+                                               id_album INT,
+                                               id_interpret INT,
+                                               FOREIGN KEY (id_album) REFERENCES album(id_album),
+    FOREIGN KEY (id_interpret) REFERENCES interpret(id_interpret)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+-- Vytvoření spojovací tabulky album_skladba
+CREATE TABLE IF NOT EXISTS album_skladba (
+                                             id_album_skladba INT PRIMARY KEY AUTO_INCREMENT,
+                                             cislo_stopy INT,
+                                             id_album INT,
+                                             id_skladba INT,
+                                             FOREIGN KEY (id_album) REFERENCES album(id_album),
+    FOREIGN KEY (id_skladba) REFERENCES skladba(id_skladba)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
---
+++ project/sql/data.sql
@@ -0,0 +1,30 @@
+-- Vložení testovacích dat
+INSERT INTO typ_zanr VALUES (1, 'Rock'), (2, 'Pop'), (3, 'Country'), (4, 'Blues'), (5, 'Jazz');
+INSERT INTO typ_narodnost VALUES (1, 'USA'), (2, 'UK'), (3, 'Czech Republic'), (4, 'Germany'), (5, 'Spain');
+INSERT INTO interpret VALUES (1, 'The Beatles', 2), (2, 'Queen', 2), (3, 'Karel Gott', 3), (4, 'BB King', 4), (5, 'Miles Davis', 1);
+INSERT INTO album VALUES (1, 1, 'Abbey Road', '1969-09-26'),
+                         (2, 2, 'A Night at the Opera', '1975-11-21'),
+                         (3, 3, 'Karel Gott - Největší hity', '2000-01-01'),
+                         (4, 4, 'BB King - Blues on Top of Blues', '1968-08-01'),
+                         (5, 5, 'Miles Davis - Kind of Blue', '1959-08-17');
+
+INSERT INTO skladba VALUES
+                        (1, 'Bohemian Rhapsody', 355),
+                        (2, 'Hey Jude', 431),
+                        (3, 'Maja', 180),
+                        (4, 'Heartbreaker', 250),
+                        (5, 'So What', 550);
+
+INSERT INTO album_interpret VALUES
+                                (1, 1, 1),
+                                (2, 2, 2),
+                                (3, 3, 3),
+                                (4, 4, 4),
+                                (5, 5, 5);
+
+INSERT INTO album_skladba VALUES
+                              (1, 1, 1, 1),
+                              (2, 2, 1, 2),
+                              (3, 3, 3, 3),
+                              (4, 4, 4, 4),
+                              (5, 5, 5, 5);
